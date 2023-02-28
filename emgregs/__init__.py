0e41dc90590786cfa869169611de551bd26df21d
jgori <juliengori@gmail.com>
2023-02-28 10:58:09 +0100

    Bump version: 0.0.3 → 0.0.3

--- emgregs/__init__.py
+++ emgregs/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "__version__ = "0.0.3""
+
 from emgregs.emg_reg import emg_reg, emg_reg_heterosked
 from emgregs.flare_reg import flare_reg
 from emgregs.simulation import sim_emg_reg, sim_emg_reg_heterosked, sim_flare_reg
