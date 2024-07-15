eba37b8a9810efe25ed6ee245fb57eefacd07d6c
jgori <juliengori@gmail.com>
2024-07-15 16:37:51 +0200

    Bump version: 0.0.6 → 0.0.7-dev0

--- emgregs/simulation.py
+++ emgregs/simulation.py
@@ -8,14 +8,18 @@ import numpy.typing as npt
 # Everywhere, lambda is replaced by k (protected word in Python)
 
 
-# Init RNG
-rng = numpy.random.default_rng(seed=123)
-
-
 def exg_pdf_residuals(
-    shape: Tuple[int] = (1,), mu: float = 0, k: float = 1, sigma: float = 1
+    shape: Tuple[int] = (1,),
+    mu: float = 0,
+    k: float = 1,
+    sigma: float = 1,
+    rng=None,
+    seed=None,
 ) -> npt.NDArray[Any]:
     """Generates random emg deviates"""
+    if rng is None:
+        rng = numpy.random.default_rng(seed=seed)
+
     return rng.normal(loc=mu, scale=sigma, size=shape) + rng.exponential(
         scale=1 / k, size=shape
     )
@@ -27,8 +31,12 @@ def exg_pdf_residuals_heterosked(
     mu: float = 0,
     expo_scale: Tuple[float] = (1, 2),
     sigma: float = 1,
+    rng=None,
+    seed=None,
 ) -> npt.NDArray[Any]:
     """Generates random emg deviates"""
+    if rng is None:
+        rng = numpy.random.default_rng(seed=seed)
     return rng.normal(loc=mu, scale=sigma, size=shape) + rng.exponential(
         scale=numpy.sum((expo_scale * x), axis=1), size=shape
     )
@@ -42,8 +50,14 @@ def sim_emg_reg_heterosked(
     beta: Tuple[float] = (2, 3, 5),  # Or array
     sigma: float = 0.5,
     expo_scale: Tuple[float] = (1, 2, 0),
+    rng=None,
+    seed=None,
 ) -> Dict[npt.NDArray[Any], npt.NDArray[Any]]:
     """Scale of the exponential is expo_scale.T @ X"""
+
+    if rng is None:
+        rng = numpy.random.default_rng(seed=seed)
+    residual_rng = rng.spawn(1)[0]
 
     expo_scale = numpy.asarray(expo_scale)
     if (expo_scale * xmin <= 0).all() or (expo_scale * xmax <= 0).all():
@@ -69,21 +83,29 @@ def sim_emg_reg_heterosked(
         X[:, 1:] = (xmax - xmin) * rng.random(size=(n, m - 1)) + xmin
 
     # Add EMG Noise
-    E = exg_pdf_residuals_heterosked(X, shape=(n,), sigma=sigma, expo_scale=expo_scale)
+    E = exg_pdf_residuals_heterosked(
+        X, shape=(n,), sigma=sigma, expo_scale=expo_scale, rng=residual_rng
+    )
     Y = (X @ beta).squeeze() + E
-    X = X[:, 1:]
+    X = X[:, 1:].squeeze()
 
     return {"X": X, "Y": Y}
 
 
 def sim_emg_reg(
-    xmin: npt.NDArray[Any] = 0.5,
+    xmin: npt.NDArray[Any] = -2,
     xmax: npt.NDArray[Any] = 4,
     n: int = 50,
     beta: Tuple[float] = (2, 3, 5),  # Or array
     sigma: float = 0.5,
     alpha: float = 0.01,
+    rng=None,
+    seed=None,
 ) -> Dict[npt.NDArray[Any], npt.NDArray[Any]]:
+
+    if rng is None:
+        rng = numpy.random.default_rng(seed=seed)
+    residual_rng = rng.spawn(1)[0]
 
     if alpha <= 0:
         raise ValueError(f"Input alpha should be strictly positive, but equals {alpha}")
@@ -96,19 +118,12 @@ def sim_emg_reg(
 
     # https://numpy.org/doc/stable/reference/random/generated/numpy.random.Generator.random.html
 
-    if n is None:
-        if X is None:
-            n = 50
-        else:
-            n = len(X)
-
-    if X is None:
-        X = numpy.zeros((n, m))
-        X[:, 0] = 1
-        X[:, 1:] = (xmax - xmin) * rng.random(size=(n, m - 1)) + xmin
+    X = numpy.zeros((n, m))
+    X[:, 0] = 1
+    X[:, 1:] = (xmax - xmin) * rng.random(size=(n, m - 1)) + xmin
 
     # Add EMG Noise
-    E = exg_pdf_residuals(shape=(n,), sigma=sigma, k=alpha)
+    E = exg_pdf_residuals(shape=(n,), sigma=sigma, k=alpha, rng=residual_rng)
     Y = (X @ beta).squeeze() + E
     X = X[:, 1:]
 
@@ -116,14 +131,19 @@ def sim_emg_reg(
 
 
 def sim_flare_reg(
-    xmin: npt.NDArray[Any] = 0.5,
+    xmin: npt.NDArray[Any] = -2,
     xmax: npt.NDArray[Any] = 4,
     n: int = 50,
     beta: Tuple[float] = (2, 3, 5),  # Or array
     k: Tuple[float] = (0.5,),
     sigma: float = 0.5,
     alpha: float = 0.01,
+    rng=None,
+    seed=None,
 ) -> Dict[npt.NDArray[Any], npt.NDArray[Any]]:
+
+    if rng is None:
+        rng = numpy.random.default_rng(seed=seed)
 
     if alpha <= 0:
         raise ValueError(f"Input alpha should be strictly positive, but equals {alpha}")
@@ -136,16 +156,9 @@ def sim_flare_reg(
 
     # https://numpy.org/doc/stable/reference/random/generated/numpy.random.Generator.random.html
 
-    if n is None:
-        if X is None:
-            n = 50
-        else:
-            n = len(X)
-
-    if X is None:
-        X = numpy.zeros((n, m))
-        X[:, 0] = 1
-        X[:, 1:] = (xmax - xmin) * rng.random(size=(n, m - 1)) + xmin
+    X = numpy.zeros((n, m))
+    X[:, 0] = 1
+    X[:, 1:] = (xmax - xmin) * rng.random(size=(n, m - 1)) + xmin
 
     # Add Noise mixture (flare)
     k_zero = k[0]
@@ -171,9 +184,9 @@ if __name__ == "__main__":
     beta = (2, 3, 5)
     sigma = 0.5
     alpha = 0.01
-    # If using a vector of input X values, it should match the size of beta. E.g. with beta = (0,1), you should use [1,x] (eg with statsmodels add_constant)
+
     x, y = sim_emg_reg(
-        xmin=xmin, xmax=xmax, n=n, beta=beta, sigma=sigma, alpha=alpha
+        xmin=xmin, xmax=xmax, n=n, beta=beta, sigma=sigma, alpha=alpha, seed=1234
     ).values()
     fig = plt.figure()
     axx = fig.add_subplot(221)
@@ -182,7 +195,7 @@ if __name__ == "__main__":
     axy.plot(range(len(y)), y, "ko")
 
     x, y = sim_flare_reg(
-        xmin=xmin, xmax=xmax, n=n, beta=beta, sigma=sigma, alpha=alpha
+        xmin=xmin, xmax=xmax, n=n, beta=beta, sigma=sigma, alpha=alpha, seed=789
     ).values()
 
     axx = fig.add_subplot(223)
